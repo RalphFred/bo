5aa0affdcae746a5676c74e94722daea8151338f
RalphFred <raphaelgbaorun@gmail.com>
2024-09-02 10:00:01 +0100

    fix: deployment issue

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import Hero from '../components/Hero';
 import Description from '../components/Description';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
+import LocomotiveScroll from 'locomotive-scroll';
 
 export default function Home() {
   const locomotiveScroll = new LocomotiveScroll();
