ea51706ecd486a5bd7be406a806c833f8d012794
RalphFred <raphaelgbaorun@gmail.com>
2024-09-03 12:11:51 +0100

    Omo

--- app/page.jsx
+++ app/page.jsx
@@ -1,37 +1,20 @@
 "use client";
-import { useEffect, useRef } from 'react';
-import style from './page.module.scss';
+import style from './page.module.scss'
 import Hero from '../components/Hero';
 import Description from '../components/Description';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
 import LocomotiveScroll from 'locomotive-scroll';
-import 'locomotive-scroll/dist/locomotive-scroll.css'; // Import LocomotiveScroll CSS
+import { useEffect } from 'react';
 
 export default function Home() {
-  const scrollRef = useRef(null);
-  const locomotiveScroll = useRef(null);
-
+  
   useEffect(() => {
-    // Ensure this code runs only on the client
-    if (typeof window !== 'undefined' && scrollRef.current) {
-      locomotiveScroll.current = new LocomotiveScroll({
-        el: scrollRef.current,
-        smooth: true,
-        // Add any other LocomotiveScroll options here
-      });
-
-      // Cleanup function to destroy LocomotiveScroll instance on unmount
-      return () => {
-        if (locomotiveScroll.current) {
-          locomotiveScroll.current.destroy();
-        }
-      };
-    }
-  }, []);
-
+    const locomotiveScroll = new LocomotiveScroll();
+  }, [])
+  
   return (
-    <main ref={scrollRef}>
+    <main>
       <Hero />
       <Description />
       <Newsletter />
