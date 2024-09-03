79a67a688b0565838af303e09313bb1b1a9d3d9c
RalphFred <raphaelgbaorun@gmail.com>
2024-09-03 12:07:45 +0100

    df

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,37 @@
 "use client";
-import style from './page.module.scss'
+import { useEffect, useRef } from 'react';
+import style from './page.module.scss';
 import Hero from '../components/Hero';
 import Description from '../components/Description';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
 import LocomotiveScroll from 'locomotive-scroll';
+import 'locomotive-scroll/dist/locomotive-scroll.css'; // Import LocomotiveScroll CSS
 
 export default function Home() {
-  const locomotiveScroll = new LocomotiveScroll();
-  
+  const scrollRef = useRef(null);
+  const locomotiveScroll = useRef(null);
+
+  useEffect(() => {
+    // Ensure this code runs only on the client
+    if (typeof window !== 'undefined' && scrollRef.current) {
+      locomotiveScroll.current = new LocomotiveScroll({
+        el: scrollRef.current,
+        smooth: true,
+        // Add any other LocomotiveScroll options here
+      });
+
+      // Cleanup function to destroy LocomotiveScroll instance on unmount
+      return () => {
+        if (locomotiveScroll.current) {
+          locomotiveScroll.current.destroy();
+        }
+      };
+    }
+  }, []);
+
   return (
-    <main>
+    <main ref={scrollRef}>
       <Hero />
       <Description />
       <Newsletter />
