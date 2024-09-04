d0ed8bd2f2c04a773f96a6f984674a584f7a5b34
RalphFred <raphaelgbaorun@gmail.com>
2024-09-04 14:16:45 +0100

    dep

--- app/page.jsx
+++ app/page.jsx
@@ -4,14 +4,14 @@ import Hero from '../components/Hero';
 import Description from '../components/Description';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
-import LocomotiveScroll from 'locomotive-scroll';
-import { useEffect } from 'react';
+// import LocomotiveScroll from 'locomotive-scroll';
+// import { useEffect } from 'react';
 
 export default function Home() {
   
-  useEffect(() => {
-    const locomotiveScroll = new LocomotiveScroll();
-  }, []);
+  // useEffect(() => {
+  //   const locomotiveScroll = new LocomotiveScroll();
+  // }, []);
   
   return (
     <main className={style.wrapper}>
